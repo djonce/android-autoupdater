2e72ea03c6bb68b4fbdd36a61f699e72117b86e2
djonce <891879872@qq.com>
2016-01-08 18:52:31 +0800

    xml

--- library/src/main/java/com/wangj/library/OnUpdateListener.java
+++ library/src/main/java/com/wangj/library/OnUpdateListener.java
@@ -1,8 +1,18 @@
 package com.wangj.library;
 
 /**
+ *
  * Created by wangj on 16/1/7.
  */
 public interface OnUpdateListener {
 
+    public void onStartCheck();
+
+    public void onFinishCheck(UpdateInfo updateInfo);
+
+    public void onStartDownload();
+
+    public void onDownloading(int progress);
+
+    public void onFinshDownload();
 }
--- library/src/main/java/com/wangj/library/UpdateInfo.java
+++ library/src/main/java/com/wangj/library/UpdateInfo.java
@@ -1,7 +1,90 @@
 package com.wangj.library;
 
+import java.util.Map;
+
 /**
- * Created by wangj on 16/1/7.
+ * Created by wangj on 16/1/7
  */
 public class UpdateInfo {
+    private String appName;
+    private String appDescription;
+    private String packageName;
+    private String versionCode;
+    private String versionName;
+    private boolean forceUpdate;
+    private boolean autoUpdate;
+    private String apkUrl;
+    private String updateTips;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppDescription() {
+        return appDescription;
+    }
+
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(String versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public boolean isForceUpdate() {
+        return forceUpdate;
+    }
+
+    public void setForceUpdate(boolean forceUpdate) {
+        this.forceUpdate = forceUpdate;
+    }
+
+    public boolean isAutoUpdate() {
+        return autoUpdate;
+    }
+
+    public void setAutoUpdate(boolean autoUpdate) {
+        this.autoUpdate = autoUpdate;
+    }
+
+    public String getApkUrl() {
+        return apkUrl;
+    }
+
+    public void setApkUrl(String apkUrl) {
+        this.apkUrl = apkUrl;
+    }
+
+    public String getUpdateTips() {
+        return updateTips;
+    }
+
+    public void setUpdateTips(String updateTips) {
+        this.updateTips = updateTips;
+    }
 }
